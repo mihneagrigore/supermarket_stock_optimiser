a42062a5c9a20c7cdbe12e73f1e0dd3f78443f38
MihutMatei <mihutmatei@gmail.com>
2025-12-28 17:59:47 +0200

    dashboard POC with minimal css
    Signed-off-by: MihutMatei <mihutmatei@gmail.com>

--- frontend/app/main_app.py
+++ frontend/app/main_app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.pages.home import home_pages
 from app.pages.login import login_pages
 from app.pages.signup import signup_pages
+from app.pages.dashboard import dashboard_pages
 import os
 import secrets
 
@@ -11,3 +12,4 @@ app.secret_key = os.getenv("SECRET_KEY") or secrets.token_hex(32)
 app.register_blueprint(home_pages)
 app.register_blueprint(login_pages)
 app.register_blueprint(signup_pages)
+app.register_blueprint(dashboard_pages)
